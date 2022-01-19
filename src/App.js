2c937f1235d647f4544bbd5a1a22d5310876147c
Henry Tran <77460898+henrytranvn93@users.noreply.github.com>
2022-01-18 20:43:38 -0800

    add place card and style into the app

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
     const [places, setPlaces] = useState([]);
 
     const [coordinates, setCoordinates] = useState({});
-    const [bounds, setBounds] = useState(null);
+    const [bounds, setBounds] = useState({});
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude}}) => {
