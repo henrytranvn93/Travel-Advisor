500453fff77f4f3ee1d5cc4d7c707e078e2c225f
Henry Tran <77460898+henrytranvn93@users.noreply.github.com>
2022-01-26 15:13:21 -0800

    adding functions to Select options and Ratings and Searching places, fix minor bugs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
 import useStyles from './headerStyles';
 
-export default function Header() {
+export default function Header({ setCoordinates }) {
     const classes = useStyles();
+    const [autocomplete, setAutoComplete] = useState(null);
+
+    const onLoad = (autoC) => setAutoComplete(autoC);
+
+    const onPlaceChanged = () => {
+        const lat = autocomplete.getPlace().geometry.location.lat();
+        const lng = autocomplete.getPlace().geometry.location.lng();
+        setCoordinates({ lat, lng });
+    }
 
     return (
         <AppBar position="static">
@@ -18,14 +27,14 @@ export default function Header() {
                     <Typography variant="h6" className={classes.title}>
                         Explore new places
                     </Typography>
-                {/* <Autocomplete> */}
+                <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
                     <div className={classes.search}>
                         <div className={classes.searchIcon}>
                             <SearchIcon/>
                         </div>
                         <InputBase placeholder="Search..." classes={{root: classes.inputRoot, input: classes.inputInput}}/>
                     </div>
-                {/* </Autocomplete> */}
+                </Autocomplete>
                 </Box>
             </Toolbar>
         </AppBar>
